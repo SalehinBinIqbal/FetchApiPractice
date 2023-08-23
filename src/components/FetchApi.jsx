bcd699552c681672b63bcec3ecc03655b5c1a483
SalehinBinIqbal <salehin.rawna@gmail.com>
2023-08-23 14:53:34 +0600

    Feat: Updated the Section to able to fetch data on buttonClick on large api

--- src/components/FetchApi.jsx
+++ src/components/FetchApi.jsx
@@ -24,7 +24,7 @@ function FetchApi() {
 
   const showData = async () => {
     try {
-      const res = await fetch("https://reqres.in/api/posts");
+      const res = await fetch("https://api.npms.io/v2/search?q=react");
       if (!res.ok) {
         throw res;
       }
@@ -39,20 +39,28 @@ function FetchApi() {
     <>
       {error && <h1>There is a error</h1>}
       <div className={classes.tableHead}>
-        <p>ID</p>
-        <p>Name</p>
-        <p>Year</p>
-        <p>Color</p>
+        <div className={classes.headWrapper}>
+          <p>Name</p>
+          <p>Email</p>
+        </div>
+        <p>Score</p>
       </div>
       <button onClick={showData}>Show Data</button>
       {data &&
-        data.data.map((info) => {
+        data.results.map((info) => {
           return (
-            <div className={classes.item} key={info.id}>
-              <p>{info.id}</p>
-              <p>{info.name}</p>
-              <p>{info.year}</p>
-              <p>{info.color}</p>
+            <div className={classes.items}>
+              <div>
+                {info.package.maintainers.map((item, index) => {
+                  return (
+                    <div className={classes.item} key={index}>
+                      <p>{item.username}</p>
+                      <p>{item.email}</p>
+                    </div>
+                  );
+                })}
+              </div>
+              <p>{info.score.final}</p>
             </div>
           );
         })}
